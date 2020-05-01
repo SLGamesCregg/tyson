795fdc3bdc5e3a461e29b720b576d3d4ee2de1e5
SLGamesCregg <theheartlandsaga@gmail.com>
2020-04-30 19:27:12 -0500

    Getting the holepunch plugin in a working state

--- holepunch-plugin/addons/Holepunch/holepunch_node.gd
+++ holepunch-plugin/addons/Holepunch/holepunch_node.gd
@@ -1,226 +1,226 @@
 extends Node
 
-signal hole_punched
+#Signal is emitted when holepunch is complete. Connect this signal to your network manager
+#Once your network manager received the signal they can host or join a game on the host port
+signal hole_punched(my_port, hosts_port, hosts_address)
 
 var server_udp = PacketPeerUDP.new()
 var peer_udp = PacketPeerUDP.new()
 
+#Set the rendevouz address to the IP address of your third party server
 export(String) var rendevouz_address = "" 
-export(int) var max_player_count = 2
-var rendevouz_port = 4000
-var found_server = false
-var peer_info = 0
-var recieved_host_info = false
-var peer_greet = 0
-var peer_confirm = 0
-var peer_go = 0
+#Set the rendevouz port to the port of your third party server
+export(int) var rendevouz_port = 4000
+#This is the range of ports you will search if you hear no response from the first port tried
+export(int) var port_cascade_range = 10
+#The amount of messages of the same type you will send before cascading or giving up
+export(int) var response_window = 5
 
-# warning-ignore:unused_class_variable
+
+var found_server = false
+var recieved_peer_info = false
+var recieved_peer_greet = false
+var recieved_peer_confirm = false
+var recieved_peer_go = false
+
 var is_host = false
 var starting_game = false
 
 var own_port
-var peers = {}
-var host_address
-var host_port
+var peer = {}
+var host_address = ""
+var host_port = 0
 var client_name
 var p_timer
-var cascade_timer
 var session_id
 
 var ports_tried = 0
 var greets_sent = 0
+var gos_sent = 0
 
 const REGISTER_SESSION = "rs:"
 const REGISTER_CLIENT = "rc:"
 const EXCHANGE_PEERS = "ep:"
+const PEER_GREET = "greet"
+const PEER_CONFIRM = "confirm"
+const PEER_GO = "go"
+const SERVER_OK = "ok"
+const SERVER_INFO = "peers"
+
+const MAX_PLAYER_COUNT = 2
 
 # warning-ignore:unused_argument
 func _process(delta):
 	if peer_udp.get_available_packet_count() > 0:
 		var array_bytes = peer_udp.get_packet()
 		var packet_string = array_bytes.get_string_from_ascii()
+		if not recieved_peer_greet:
+			if packet_string.begins_with(PEER_GREET):
+				var m = packet_string.split(":")
+				_handle_greet_message(m[1], int(m[2]), int(m[3]))
 
-				
-		if peer_greet < peer_info:
-			if packet_string.begins_with("greet"):
+		if not recieved_peer_confirm:
+			if packet_string.begins_with(PEER_CONFIRM):
 				var m = packet_string.split(":")
-				if !peers[m[3]].has("name"):
-					peer_greet += 1
-					var peer_used_port = m[2]
-					peers[m[3]].name = m[1]
-					if own_port != peer_used_port:
-						own_port = peer_used_port
-						print("Changing own port to: " + str(own_port))
-						peer_udp.close()
-						peer_udp.listen(own_port, "*")
-						
-					print("Sending confirm")
-					
-				
-		if peer_confirm < peer_info:
-			if packet_string.begins_with("confirm"):
-				var m = packet_string.split(":")
-				if !peers[m[2]].has("is_host"):
-					var confirmed_port = int(m[1])
-					print("Recieved confirm from: "+peers[m[2]].name + "for port: " + str(confirmed_port))
-					if peers[m[2]].port != confirmed_port:
-						peers[m[2]].port = confirmed_port
-						print("Changing peer port to: " + str(own_port))
-						peer_udp.set_dest_address(peers[m[2]], peers[m[2]].port)
-						cascade_timer.stop()
-						
-					peers[m[2]].is_host = m[3]
-					peers[m[2]].port = confirmed_port
-					if peers[m[2]].is_host:
-						host_address = peers[m[2]]
-						host_port = peers[m[2]].port
-					peer_udp.close()
-					peer_udp.listen(peers[m[2]].port, "*")
-					print("Sending go")
-					peer_confirm += 1
-					peer_udp.close()
+				_handle_confirm_message(m[2], m[1], m[4], m[3])
 
-				
-		elif peer_go < peer_info:
-			if packet_string.begins_with("go"):
+		elif not recieved_peer_go:
+			if packet_string.begins_with(PEER_GO):
 				var m = packet_string.split(":")
-				if !peers[m[1]].has("go"):
-					print("Recieved go from: "+m[1])
-					peer_go += 1
-			if peer_go == peer_info:
-				emit_signal("hole_punched")
-				set_process(false)
-		
+				_handle_go_message(m[1])
+
 	if server_udp.get_available_packet_count() > 0:
 		var array_bytes = server_udp.get_packet()
 		var packet_string = array_bytes.get_string_from_ascii()
-		print("Recieved Packet from server: " + packet_string)
-		
-		if not found_server:
-			if packet_string.begins_with("ok"):
-				found_server=true
-				print("Recieved ok from Rendezvous Server")
-				var m = packet_string.split(":")
-				own_port =int( m[1] )
-				print("Own port is: " + str(own_port))
-				print("Waiting for peer...")
-				if is_host:
-					emit_signal("port_updated")
+		if packet_string.begins_with(SERVER_OK):
+			var m = packet_string.split(":")
+			own_port = int( m[1] )
+			if is_host:
+				if !found_server:
 					_send_client_to_server()
-					
-				
-		elif peer_info == 0:
-			if packet_string.begins_with("peer:"):
-				var peer_list = packet_string.split(",")
-				print("Recieved peer info")
-				for i in peer_list:
-					var m = packet_string.split(":")
-					peers[m[1]] = {"port":m[2]}
-					peer_info += 1
-					print("Peer adress: " + m[1])
-					print("Peer port: " + str( m[2]))
-				start_peer_contact()
-				
-#func cascade_peer():
-#	if peer_confirm < peer_info:
-#		print("pinging on port " + str(ports_tried + peer_port) + " and above")
-#		for i in range(ports_tried, ports_tried+10):
-#			var p = peer_port + i
-#			peer_udp.set_dest_address(peer_address, p)
-#			var buffer = PoolByteArray()
-#			buffer.append_array(("greet:"+ client_name+":"+str(p) ).to_utf8())
-#			peer_udp.put_packet(buffer)
-#			yield(get_tree(),"idle_frame")
-#		ports_tried+=10
-	
+			found_server=true
 
-func ping_peer():
-#	print("pinging peer...")
+		if not recieved_peer_info:
+			if packet_string.begins_with(SERVER_INFO):
+				server_udp.close()
+				packet_string = packet_string.right(6)
+				if packet_string.length() > 2:
+					var m = packet_string.split(":")
+					peer[m[0]] = {"port":m[2], "address":m[1]}
+					recieved_peer_info = true
+					start_peer_contact()
+
+
+func _handle_greet_message(peer_name, peer_port, my_port):
+	if own_port != my_port:
+		own_port = my_port
+		peer_udp.close()
+		peer_udp.listen(own_port, "*")
+	recieved_peer_greet = true
+
+
+func _handle_confirm_message(peer_name, peer_port, my_port, is_host):
+	if peer[peer_name].port != peer_port:
+		peer[peer_name].port = peer_port
+
+	peer[peer_name].is_host = is_host
+	if is_host:
+		host_address = peer[peer_name].address
+		host_port = peer[peer_name].port
+	peer_udp.close()
+	peer_udp.listen(own_port, "*")
+	recieved_peer_confirm = true
+
+
+func _handle_go_message(peer_name):
+	recieved_peer_go = true
+	emit_signal("hole_punched", own_port, host_port, host_address)
+	peer_udp.close()
+	set_process(false)
+
+
+func _cascade_peer(add, peer_port):
+	for i in range(peer_port - port_cascade_range, peer_port + port_cascade_range):
+		peer_udp.set_dest_address(add, i)
+		var buffer = PoolByteArray()
+		buffer.append_array(("greet:"+client_name+":"+str(own_port)+":"+str(i)).to_utf8())
+		peer_udp.put_packet(buffer)
+		ports_tried += 1
+
+
+func _ping_peer():
 	if starting_game: return
-	var buffer
 	
-	if peer_confirm < peer_info and greets_sent < 7:
-#		print("sending:"+ str(buffer.get_string_from_utf8()))
-		for p in peers.keys():
-			peer_udp.set_dest_address(p, peers[p].port)
-			buffer = PoolByteArray()
-			buffer.append_array(("greet:"+ client_name+":"+str(peers[p].port)+":"+p.to_utf8()))
+	if not recieved_peer_confirm and greets_sent < response_window:
+		for p in peer.keys():
+			peer_udp.set_dest_address(peer[p].address, int(peer[p].port))
+			var buffer = PoolByteArray()
+			buffer.append_array(("greet:"+client_name+":"+str(own_port)+":"+peer[p].port).to_utf8())
 			peer_udp.put_packet(buffer)
 			greets_sent+=1
-			#if greets_sent == 6:
-				#print("Receiving no confirm. Starting port cascade")
-				#.start()
-		
-	elif peer_greet == peer_info and peer_go < peer_info:
-		for p in peers.keys():
-			peer_udp.set_dest_address(p, peers[p].port)
-			buffer = PoolByteArray()
-			buffer.append_array(("confirm:" + str(own_port)+":"+p+":"+str(is_host)).to_utf8())
-	#		print("sending:"+ str(buffer.get_string_from_utf8()))
+			if greets_sent == response_window:
+				print("Receiving no confirm. Starting port cascade")
+				#if the other player hasn't responded we should try more ports
+
+
+	if not recieved_peer_confirm and greets_sent == response_window:
+		for p in peer.keys():
+			_cascade_peer(peer[p].address, int(peer[p].port))
+		greets_sent += 1
+
+	if recieved_peer_greet and not recieved_peer_go:
+		for p in peer.keys():
+			peer_udp.set_dest_address(peer[p].address, int(peer[p].port))
+			var buffer = PoolByteArray()
+			buffer.append_array(("confirm:"+str(own_port)+":"+client_name+":"+str(is_host)+":"+peer[p].port).to_utf8())
 			peer_udp.put_packet(buffer)
-		
-	if  peer_confirm == peer_info:
-		for p in peers.keys():
-			peer_udp.set_dest_address(p, peers[p].port)
-			buffer = PoolByteArray()
-			buffer.append_array(("go:"+p).to_utf8())
-	#		print("sending:"+ str(buffer.get_string_from_utf8()))
+
+	if  recieved_peer_confirm:
+		for p in peer.keys():
+			peer_udp.set_dest_address(peer[p].address, int(peer[p].port))
+			var buffer = PoolByteArray()
+			buffer.append_array(("go:"+client_name).to_utf8())
 			peer_udp.put_packet(buffer)
+		gos_sent += 1
+
+		if gos_sent >= response_window: #the other player has confirmed and is probably waiting
+			emit_signal("hole_punched")
+			p_timer.stop()
+			set_process(false)
+
 
 func start_peer_contact():	
 	server_udp.put_packet("goodbye".to_utf8())
 	server_udp.close()
 	if peer_udp.is_listening():
 		peer_udp.close()
-	#peer_udp.set_dest_address(peer_address, peer_port)
-	
 	var err = peer_udp.listen(own_port, "*")
 	if err != OK:
-		print("Error listening on port: " + str(own_port))
-	else:
-		print("Listening on port: " + str(own_port))
+		print("Error listening on port: " + str(own_port) +" Error: " + str(err))
 	p_timer.start()
-	print("Contacting peer...")
-	
+
+
+#this function can be called to the server if you want to end the holepunch before the server closes the session
 func finalize_peers(id):
 	var buffer = PoolByteArray()
 	buffer.append_array((EXCHANGE_PEERS+str(id)).to_utf8())
-	server_udp.close()
 	server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 	server_udp.put_packet(buffer)
-	
-func start_traversal(id):
+
+
+#Call this function when you want to start the holepunch process
+func start_traversal(id, is_player_host, player_name):
 	if server_udp.is_listening():
 		server_udp.close()
+
 	var err = server_udp.listen(rendevouz_port, "*")
 	if err != OK:
 		print("Error listening on port: " + str(rendevouz_port) + " to server: " + rendevouz_address)
-	else:
-		print("Listening on port: " + str(rendevouz_port) + " to server: " + rendevouz_address)
-		
+
+	is_host = is_player_host
+	client_name = player_name
 	found_server = false
-	peer_info = 0
-	peer_greet = 0
-	peer_confirm = 0
-	peer_go = 0
-	peers = {}
+	recieved_peer_info = false
+	recieved_peer_greet = false
+	recieved_peer_confirm = false
+	recieved_peer_go = false
+	peer = {}
 
 	ports_tried = 0
 	greets_sent = 0
+	gos_sent = 0
 	session_id = id
-
-	print("Connecting Rendezvouz Server...")
 	
 	if (is_host):
 		var buffer = PoolByteArray()
-		buffer.append_array((REGISTER_SESSION+session_id+":"+str(max_player_count)).to_utf8())
+		buffer.append_array((REGISTER_SESSION+session_id+":"+str(MAX_PLAYER_COUNT)).to_utf8())
 		server_udp.close()
 		server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 		server_udp.put_packet(buffer)
 	else:
 		_send_client_to_server()
-	
+
+
 func _send_client_to_server():
 	yield(get_tree().create_timer(2.0), "timeout")
 	var buffer = PoolByteArray()
@@ -228,19 +228,14 @@ func _send_client_to_server():
 	server_udp.close()
 	server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 	server_udp.put_packet(buffer)
-		
+
+
 func _exit_tree():
 	server_udp.close()
-	
-#func print(line):	get_node("/root/Lobby").print(line)
-	
+
+
 func _ready():
 	p_timer = Timer.new()
 	get_node("/root/").add_child(p_timer)
-	p_timer.connect("timeout", self, "ping_peer")
-	p_timer.wait_time = 0.3
-	
-#	cascade_timer = Timer.new()
-#	get_node("/root/").add_child(cascade_timer)
-#	cascade_timer.connect("timeout", self, "cascade_peer")
-#	cascade_timer.wait_time = 0.2
+	p_timer.connect("timeout", self, "_ping_peer")
+	p_timer.wait_time = 0.1
